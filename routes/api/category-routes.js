3e209e5174f308e79d5db7d6ed9149009729d598
LemonPocky <someone-70@live.com>
2021-07-24 18:44:40 -0700

    Added GET all categories API route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,9 +3,22 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll({
+      // JOIN with Products
+      include: [
+        {
+          model: Product,
+        },
+      ],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
