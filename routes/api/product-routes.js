c23a246eb19579a1939106fff261ca4bb0184f0b
LemonPocky <someone-70@live.com>
2021-07-24 17:04:47 -0700

    Added get and delete routes for Product API

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -32,9 +32,37 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      // JOIN with Category and ProductTag
+      include: [
+        {
+          model: Category,
+        },
+        {
+          model: Tag,
+          through: {
+            model: ProductTag,
+            // Hides the columns from ProductTag
+            // The end user doesn't need to know the exact values in this table
+            attributes: [],
+          },
+        },
+      ],
+    });
+
+    // Send a 404 if there's no id match
+    if (!productData) {
+      res.status(404).json({ message: "No product found with this id!" });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -121,8 +149,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    // Send a 404 if there's no id match
+    if (!productData) {
+      res.status(404).json({ message: "No product found with this id!" });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
